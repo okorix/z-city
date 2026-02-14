bea254e8e73b11c5ecf2be4cf5a142aaea83c321
Okorix <HZZVvv@outlook.com>
2026-02-14 17:49:51 +0400

    cyanide canister is spawnable

--- lua/entities/ent_hg_cyanide_canister/shared.lua
+++ lua/entities/ent_hg_cyanide_canister/shared.lua
@@ -1,6 +1,6 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 ENT.PrintName = "Cyanide canister"
-ENT.Spawnable = false
+ENT.Spawnable = true
 ENT.totalparticles = 30
 ENT.Model = "models/jordfood/jtun.mdl"
